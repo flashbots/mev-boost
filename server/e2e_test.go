93c2b3f5227e6a051d1b558f086ad41a8d8edd46
Chris Hager <chris@linuxuser.at>
2022-04-27 17:54:32 +0200

    types cleanup

--- server/e2e_test.go
+++ server/e2e_test.go
@@ -67,9 +67,9 @@ func TestE2E_RegisterValidator(t *testing.T) {
 
 	res := ""
 	message := types.RegisterValidatorRequestMessage{
-		FeeRecipient: "0xdb65fEd33dc262Fe09D9a2Ba8F80b329BA25f941",
-		Timestamp:    "0x625481c2",
-		Pubkey:       "0xf9716c94aab536227804e859d15207aa7eaaacd839f39dcbdb5adc942842a8d2fb730f9f49fc719fdb86f1873e0ed1c2",
+		FeeRecipient: common.HexToAddress("0xdb65fEd33dc262Fe09D9a2Ba8F80b329BA25f941"),
+		Timestamp:    1234356,
+		Pubkey:       _hexToBytes("0xf9716c94aab536227804e859d15207aa7eaaacd839f39dcbdb5adc942842a8d2fb730f9f49fc719fdb86f1873e0ed1c2"),
 	}
 	err = client.Call(&res, "builder_registerValidatorV1",
 		message,
@@ -156,15 +156,41 @@ func TestE2E_RegisterValidator_RelayError(t *testing.T) {
 	res := false
 	err = client.Call(&res, "builder_registerValidatorV1",
 		types.RegisterValidatorRequestMessage{
-			FeeRecipient: "0xdb65fEd33dc262Fe09D9a2Ba8F80b329BA25f941",
-			Timestamp:    "0x123",
-			Pubkey:       "0xf9716c94aab536227804e859d15207aa7eaaacd839f39dcbdb5adc942842a8d2fb730f9f49fc719fdb86f1873e0ed1c2",
+			FeeRecipient: common.HexToAddress("0xdb65fEd33dc262Fe09D9a2Ba8F80b329BA25f941"),
+			Timestamp:    1234567,
+			Pubkey:       _hexToBytes("0xf9716c94aab536227804e859d15207aa7eaaacd839f39dcbdb5adc942842a8d2fb730f9f49fc719fdb86f1873e0ed1c2"),
 		},
 		"0xab5dc3c47ea96503823f364c4c1bb747560dc8874d90acdd0cbcfe1abc5457a70ab7e8175c074ace44dead2427e6d2353184c61c6eebc3620b8cec1e9115e35e4513369d7a68d7a5dad719cb6f5a85788490f76ca3580758042da4d003ef373f", // signature
 	)
 	require.NotNil(t, err, err)
 	assert.Equal(t, relay1.RequestCounter["builder_registerValidatorV1"], 1)
 	assert.Equal(t, relay2.RequestCounter["builder_registerValidatorV1"], 1)
+}
+
+// builder for a getHeader handler with a custom value
+func makeBuilderGetHeaderV1Handler(value int64, parentHash common.Hash, delay time.Duration) func(req *jsonrpc.JSONRPCRequest) (any, error) {
+	_value := big.NewInt(value)
+	return func(req *jsonrpc.JSONRPCRequest) (any, error) {
+		if delay > 0 {
+			time.Sleep(delay)
+		}
+		if len(req.Params) != 3 {
+			return nil, fmt.Errorf("Expected 3 params, got %d", len(req.Params))
+		}
+		resp := &types.GetHeaderResponse{
+			Message: types.GetHeaderResponseMessage{
+				Header: types.ExecutionPayloadHeaderV1{
+					ParentHash:    parentHash,
+					BlockHash:     common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000001"),
+					BaseFeePerGas: big.NewInt(4),
+				},
+				Value:  (*hexutil.Big)(_value),
+				Pubkey: _hexToBytes("0x1bf4b68731b493e9474f0cd5d0faaeed8796ac77267d93949c96cff6dcfaf04e49f32a7ebce3ba132b58b6f17ec5754a"),
+			},
+			Signature: _hexToBytes("0x258c63ecdc711c29f11174ebc67bcbb46efa2bda3f1a6315a28bb74b1e6d8a442778c4880f1c2e9cdaca8e3a1370a9e203fcbf45660edc61011ae9df66912c133fdaa15a917041bc7807326b42db8e6c052a7e9cb5ca9c17181952837809bb51"),
+		}
+		return resp, nil
+	}
 }
 
 func TestE2E_GetHeader(t *testing.T) {
@@ -178,43 +204,18 @@ func TestE2E_GetHeader(t *testing.T) {
 
 	parentHash := common.HexToHash("0xf254722f498df7e396694ed71f363c535ae1b2620afeaf57515e7593ad888331")
 
-	// builder for a getHeader handler with a custom value
-	makeBuilderGetHeaderV1Handler := func(value *big.Int, delay time.Duration) func(req *jsonrpc.JSONRPCRequest) (any, error) {
-		return func(req *jsonrpc.JSONRPCRequest) (any, error) {
-			if delay > 0 {
-				time.Sleep(delay)
-			}
-			if len(req.Params) != 3 {
-				return nil, fmt.Errorf("Expected 3 params, got %d", len(req.Params))
-			}
-			assert.Equal(t, parentHash.String(), req.Params[2].(string))
-			resp := &types.GetHeaderResponse{
-				Message: types.GetHeaderResponseMessage{
-					Header: types.ExecutionPayloadHeaderV1{
-						ParentHash:    parentHash,
-						BlockHash:     common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000001"),
-						BaseFeePerGas: big.NewInt(4),
-					},
-					Value:  value,
-					Pubkey: _hexToBytes("0x1bf4b68731b493e9474f0cd5d0faaeed8796ac77267d93949c96cff6dcfaf04e49f32a7ebce3ba132b58b6f17ec5754a"),
-				},
-				Signature: _hexToBytes("0x258c63ecdc711c29f11174ebc67bcbb46efa2bda3f1a6315a28bb74b1e6d8a442778c4880f1c2e9cdaca8e3a1370a9e203fcbf45660edc61011ae9df66912c133fdaa15a917041bc7807326b42db8e6c052a7e9cb5ca9c17181952837809bb51"),
-			}
-			return resp, nil
-		}
-	}
-
 	// Set handlers with different values
-	relay1.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(big.NewInt(12345), 0))
-	relay2.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(big.NewInt(12345678), 0))
+	relay1.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(12345, parentHash, 0))
+	relay2.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(12345678, parentHash, 0))
 
 	res := new(types.GetHeaderResponse)
 	pubkey := "0xf0d89fec26f5e1e84884a293677ee7e7d48505a43d23f7e4888206a780fe33ccaf374b317eb78c7036cb6c97af1dfe9a"
-	err = client.Call(&res, "builder_getHeaderV1", "0x1", pubkey, parentHash.Hex())
+	err = client.Call(&res, "builder_getHeaderV1", "0x1", pubkey, parentHash)
 	require.Nil(t, err, err)
+	require.Equal(t, parentHash, res.Message.Header.ParentHash)
 	assert.Equal(t, relay1.RequestCounter["builder_getHeaderV1"], 1)
 	assert.Equal(t, relay2.RequestCounter["builder_getHeaderV1"], 1)
-	assert.Equal(t, "12345678", res.Message.Value.String())
+	assert.Equal(t, "12345678", res.Message.Value.ToInt().String())
 
 	// ---
 	// Test with a slow relay - ensuring that a specific GetHeaderTimeout is respected.
@@ -226,16 +227,19 @@ func TestE2E_GetHeader(t *testing.T) {
 	client2 := gethRpc.DialInProc(server2)
 	defer client2.Close()
 
-	relay2.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(big.NewInt(12345678), 110*time.Millisecond))
+	relay2.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(12345678, parentHash, 110*time.Millisecond))
 	err = client2.Call(&res, "builder_getHeaderV1", "0x1", pubkey, parentHash.Hex())
 	require.Nil(t, err, err)
 	assert.Equal(t, relay1.RequestCounter["builder_getHeaderV1"], 2)
 	assert.Equal(t, relay2.RequestCounter["builder_getHeaderV1"], 2)
-	assert.Equal(t, "12345", res.Message.Value.String())
+	assert.Equal(t, "12345", res.Message.Value.ToInt().String())
 }
 
 func TestE2E_GetHeaderError(t *testing.T) {
+	parentHash := common.HexToHash("0xf254722f498df7e396694ed71f363c535ae1b2620afeaf57515e7593ad888331")
 	relay1 := setupMockRelay()
+	relay1.SetHandler("builder_getHeaderV1", makeBuilderGetHeaderV1Handler(12345, parentHash, 0))
+
 	server, err := newTestBoostRPCServer([]string{relay1.URL})
 	require.Nil(t, err, err)
 	defer server.Stop()
@@ -244,9 +248,9 @@ func TestE2E_GetHeaderError(t *testing.T) {
 	defer client.Close()
 
 	res := new(types.GetHeaderResponse)
-	err = client.Call(&res, "builder_getHeaderV1", nil, nil, nil)
+	err = client.Call(&res, "builder_getHeaderV1", "0x1", "0x1bf4b68731b493e9474f0cd5d0faaeed8796ac77267d93949c96cff6dcfaf04e49f32a7ebce3ba132b58b6f17ec575", "0xf254722f498df7e396694ed71f363c535ae1b2620afeaf57515e7593ad888331")
 	require.Error(t, err)
-	require.Equal(t, errInvalidSlot.Error(), err.Error())
+	require.Equal(t, errInvalidPubkey.Error(), err.Error())
 }
 
 func TestE2E_GetPayload(t *testing.T) {
