44df2cafebc5848ddb737910d322eb15357018d1
Justin Traglia <95511699+jtraglia@users.noreply.github.com>
2022-07-07 02:11:26 -0500

    Move mergemock-integration to separate bash script (#193)
    * Move mergemock-integration to separate bash script
    
    * Make script return 0 on success
    
    * Update CI test workflow
    
    * Allow mergemock to be run in dev mode
    
    * Rename PID variables
    
    * Simplify cleanup function

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
-MERGEMOCK_DIR=../mergemock
-MERGEMOCK_BIN=./mergemock
-
 VERSION ?= $(shell git describe --tags --always --dirty="-dev")
 DOCKER_REPO := flashbots/mev-boost
 
@@ -45,23 +42,11 @@ cover-html:
 run:
 	./mev-boost
 
-run-boost-with-relay:
-	./mev-boost -mainnet -relays http://0x821961b64d99b997c934c22b4fd6109790acf00f7969322c4e9dbf1ca278c333148284c01c5ef551a1536ddd14b178b9@127.0.0.1:28545
-
 run-dev:
 	go run cmd/mev-boost/main.go
 
-run-mergemock-engine:
-	cd $(MERGEMOCK_DIR) && $(MERGEMOCK_BIN) engine --listen-addr 127.0.0.1:8551
-
-run-mergemock-consensus:
-	cd $(MERGEMOCK_DIR) && $(MERGEMOCK_BIN) consensus --slot-time=4s --engine http://127.0.0.1:8551 --builder http://127.0.0.1:18550 --slot-bound 10
-
-run-mergemock-relay:
-	cd $(MERGEMOCK_DIR) && $(MERGEMOCK_BIN) relay --listen-addr 127.0.0.1:28545 --secret-key 1e64a14cb06073c2d7c8b0b891e5dc3dc719b86e5bf4c131ddbaa115f09f8f52
-
 run-mergemock-integration: build
-	make -j3 run-boost-with-relay run-mergemock-consensus run-mergemock-relay
+	./scripts/run_mergemock_integration.sh
 
 build-for-docker:
 	GOOS=linux go build -ldflags "-X main.version=${VERSION}" -v -o mev-boost ./cmd/mev-boost
---
+++ scripts/run_mergemock_integration.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -euo pipefail
+
+PROJECT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
+
+#
+# Default to these values for mergemock.
+#
+MERGEMOCK_DIR=${MERGEMOCK_DIR:-$PROJECT_DIR/../mergemock}
+MERGEMOCK_BIN=${MERGEMOCK_BIN:-./mergemock}
+
+#
+# This function will ensure there are no lingering processes afterwards.
+#
+cleanup() {
+  for pid in $BOOST_WITH_RELAY_PID $MERGEMOCK_RELAY_PID $MERGEMOCK_CONSENSUS_PID; do
+    if ps -p $pid &>/dev/null; then
+      disown $pid
+      kill -9 $pid &>/dev/null
+    fi
+  done
+}
+trap cleanup exit
+
+#
+# Start mev-boost.
+#
+$PROJECT_DIR/mev-boost -mainnet -relays http://0x821961b64d99b997c934c22b4fd6109790acf00f7969322c4e9dbf1ca278c333148284c01c5ef551a1536ddd14b178b9@127.0.0.1:28545 &
+BOOST_WITH_RELAY_PID=$!
+echo "Waiting for mev-boost to become available..."
+while ! nc -z localhost 18550; do
+  sleep 0.1
+done
+
+#
+# Mock the relay.
+#
+pushd $MERGEMOCK_DIR >/dev/null
+$MERGEMOCK_BIN relay --listen-addr 127.0.0.1:28545 --secret-key 1e64a14cb06073c2d7c8b0b891e5dc3dc719b86e5bf4c131ddbaa115f09f8f52 &
+MERGEMOCK_RELAY_PID=$!
+echo "Waiting for relay to become available..."
+while ! nc -z localhost 28545; do
+  sleep 0.1
+done
+
+#
+# Mock the consensus.
+#
+$MERGEMOCK_BIN consensus --slot-time=4s --engine http://127.0.0.1:8551 --builder http://127.0.0.1:18550 --slot-bound 10 &
+MERGEMOCK_CONSENSUS_PID=$!
+popd >/dev/null
+
+#
+# The script will exit when this process finishes.
+#
+wait $MERGEMOCK_CONSENSUS_PID
