c16c856dc8176e1e62960a859f5288edc40eb6f5
Justin Traglia <jtraglia@pm.me>
2022-07-06 20:59:56 -0500

    Rename PID variables

--- scripts/run_mergemock_integration.sh
+++ scripts/run_mergemock_integration.sh
@@ -12,21 +12,21 @@ MERGEMOCK_BIN=${MERGEMOCK_BIN:-./mergemock}
 #
 # This function will ensure there are no lingering processes afterwards.
 #
-RUN_BOOST_WITH_RELAY_PID=
-RUN_MERGEMOCK_RELAY_PID=
-RUN_MERGEMOCK_CONSENSUS_PID=
+BOOST_WITH_RELAY_PID=
+MERGEMOCK_RELAY_PID=
+MERGEMOCK_CONSENSUS_PID=
 cleanup() {
-  if ps -p $RUN_BOOST_WITH_RELAY_PID &>/dev/null; then
-    disown $RUN_BOOST_WITH_RELAY_PID
-    kill -9 $RUN_BOOST_WITH_RELAY_PID &>/dev/null
+  if ps -p $BOOST_WITH_RELAY_PID &>/dev/null; then
+    disown $BOOST_WITH_RELAY_PID
+    kill -9 $BOOST_WITH_RELAY_PID &>/dev/null
   fi
-  if ps -p $RUN_MERGEMOCK_RELAY_PID &>/dev/null; then
-    disown $RUN_MERGEMOCK_RELAY_PID
-    kill -9 $RUN_MERGEMOCK_RELAY_PID &>/dev/null
+  if ps -p $MERGEMOCK_RELAY_PID &>/dev/null; then
+    disown $MERGEMOCK_RELAY_PID
+    kill -9 $MERGEMOCK_RELAY_PID &>/dev/null
   fi
-  if ps -p $RUN_MERGEMOCK_CONSENSUS_PID &>/dev/null; then
-    disown $RUN_MERGEMOCK_CONSENSUS_PID
-    kill -9 $RUN_MERGEMOCK_CONSENSUS_PID &>/dev/null
+  if ps -p $MERGEMOCK_CONSENSUS_PID &>/dev/null; then
+    disown $MERGEMOCK_CONSENSUS_PID
+    kill -9 $MERGEMOCK_CONSENSUS_PID &>/dev/null
   fi
 }
 trap cleanup exit
@@ -35,7 +35,7 @@ trap cleanup exit
 # Start mev-boost.
 #
 $PROJECT_DIR/mev-boost -mainnet -relays http://0x821961b64d99b997c934c22b4fd6109790acf00f7969322c4e9dbf1ca278c333148284c01c5ef551a1536ddd14b178b9@127.0.0.1:28545 &
-RUN_BOOST_WITH_RELAY_PID=$!
+BOOST_WITH_RELAY_PID=$!
 echo "Waiting for mev-boost to become available..."
 while ! nc -z localhost 18550; do
   sleep 0.1
@@ -46,7 +46,7 @@ done
 #
 pushd $MERGEMOCK_DIR >/dev/null
 $MERGEMOCK_BIN relay --listen-addr 127.0.0.1:28545 --secret-key 1e64a14cb06073c2d7c8b0b891e5dc3dc719b86e5bf4c131ddbaa115f09f8f52 &
-RUN_MERGEMOCK_RELAY_PID=$!
+MERGEMOCK_RELAY_PID=$!
 echo "Waiting for relay to become available..."
 while ! nc -z localhost 28545; do
   sleep 0.1
@@ -56,10 +56,10 @@ done
 # Mock the consensus.
 #
 $MERGEMOCK_BIN consensus --slot-time=4s --engine http://127.0.0.1:8551 --builder http://127.0.0.1:18550 --slot-bound 10 &
-RUN_MERGEMOCK_CONSENSUS_PID=$!
+MERGEMOCK_CONSENSUS_PID=$!
 popd >/dev/null
 
 #
 # The script will exit when this process finishes.
 #
-wait $RUN_MERGEMOCK_CONSENSUS_PID
+wait $MERGEMOCK_CONSENSUS_PID
