950e973421880458e9158bd02f99206cd56cb39b
Chris Hager <chris@linuxuser.at>
2022-09-12 16:08:01 +0200

    v1.0.1-dev

--- config/vars.go
+++ config/vars.go
@@ -7,7 +7,7 @@ import (
 // Set during build
 var (
 	// Version is the version of the software, set at build time
-	Version = "v1.0.0"
+	Version = "v1.0.1-dev"
 
 	// BuildTime is the output of `date` at build time. Eg. "Sat Jul 16 13:18:55 CEST 2022"
 	BuildTime string
