9ba224521aab22c11f9e6fb034f7e9d26cb2e7ce
Justin Traglia <jtraglia@pm.me>
2022-09-23 14:49:12 -0500

    Add duplication detection for new flags

--- cli/types.go
+++ cli/types.go
@@ -4,7 +4,10 @@ import (
 	"github.com/flashbots/mev-boost/server"
 	"strings"
 	"net/url"
+	"errors"
 )
+
+var ErrDuplicateEntry = errors.New("duplicate entry")
 
 type relayList []server.RelayEntry
 
@@ -12,10 +15,22 @@ func (r *relayList) String() string {
 	return strings.Join(server.RelayEntriesToStrings(*r), ",")
 }
 
+func (r *relayList) Contains(relay server.RelayEntry) bool {
+	for _, entry := range *r {
+		if relay.String() == entry.String() {
+			return true
+		}
+	}
+	return false
+}
+
 func (r *relayList) Set(value string) error {
 	relay, err := server.NewRelayEntry(value)
 	if err != nil {
 		return err
+	}
+	if r.Contains(relay) {
+		return ErrDuplicateEntry
 	}
 	*r = append(*r, relay)
 	return nil
@@ -31,10 +46,22 @@ func (rm *relayMonitorList) String() string {
 	return strings.Join(relayMonitors, ",")
 }
 
+func (rm *relayMonitorList) Contains(relayMonitor *url.URL) bool {
+	for _, entry := range *rm {
+		if relayMonitor.String() == entry.String() {
+			return true
+		}
+	}
+	return false
+}
+
 func (rm *relayMonitorList) Set(value string) error {
 	relayMonitor, err := url.Parse(value)
 	if err != nil {
 		return err
+	}
+	if rm.Contains(relayMonitor) {
+		return ErrDuplicateEntry
 	}
 	*rm = append(*rm, relayMonitor)
 	return nil
