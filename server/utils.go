193920709f3afa0c988be10df3ecbecadee04f10
Dmarz <danmarzec@icloud.com>
2022-10-01 13:53:57 -0700

    accept gzip encoding

--- server/utils.go
+++ server/utils.go
@@ -37,8 +37,9 @@ func SendHTTPRequest(ctx context.Context, client http.Client, method, url string
 		}
 		req, err = http.NewRequestWithContext(ctx, method, url, bytes.NewReader(payloadBytes))
 
-		// Set content-type
+		// Set headers
 		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("accept-encoding", "gzip, deflate")
 	}
 	if err != nil {
 		return 0, fmt.Errorf("could not prepare request: %w", err)
