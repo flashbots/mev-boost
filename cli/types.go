504ecfcc85d940e6961d00e7a2148eb6e6cc7a11
Marius van der Wijden <m.vanderwijden@live.de>
2024-04-03 10:45:52 +0200

    server: move mock relay into mock package

--- cli/types.go
+++ cli/types.go
@@ -5,18 +5,18 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/flashbots/mev-boost/server"
+	"github.com/flashbots/mev-boost/server/types"
 )
 
 var errDuplicateEntry = errors.New("duplicate entry")
 
-type relayList []server.RelayEntry
+type relayList []types.RelayEntry
 
 func (r *relayList) String() string {
-	return strings.Join(server.RelayEntriesToStrings(*r), ",")
+	return strings.Join(types.RelayEntriesToStrings(*r), ",")
 }
 
-func (r *relayList) Contains(relay server.RelayEntry) bool {
+func (r *relayList) Contains(relay types.RelayEntry) bool {
 	for _, entry := range *r {
 		if relay.String() == entry.String() {
 			return true
@@ -26,7 +26,7 @@ func (r *relayList) Contains(relay server.RelayEntry) bool {
 }
 
 func (r *relayList) Set(value string) error {
-	relay, err := server.NewRelayEntry(value)
+	relay, err := types.NewRelayEntry(value)
 	if err != nil {
 		return err
 	}
