5342231892422b1721ef8f99d099f4e14ae4cd93
Justin Traglia <jtraglia@pm.me>
2025-02-05 16:50:21 -0600

    Address review feedback

--- server/get_header.go
+++ server/get_header.go
@@ -51,6 +51,10 @@ func (m *BoostService) getHeader(log *logrus.Entry, slot phase0.Slot, pubkey, pa
 		"msIntoSlot":  msIntoSlot,
 	}).Infof("getHeader request start - %d milliseconds into slot %d", msIntoSlot, slot)
 
+	// Get the optional version, used with SSZ decoding
+	ethConsensusVersion := header.Get("Eth-Consensus-Version")
+	log = log.WithField("ethConsensusVersion", ethConsensusVersion)
+
 	var (
 		mu sync.Mutex
 		wg sync.WaitGroup
@@ -72,53 +76,57 @@ func (m *BoostService) getHeader(log *logrus.Entry, slot phase0.Slot, pubkey, pa
 			url := relay.GetURI(fmt.Sprintf("/eth/v1/builder/header/%d/%s/%s", slot, parentHashHex, pubkey))
 			log := log.WithField("url", url)
 
-			// Build the new request
-			req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
-			if err != nil {
-				log.WithError(err).Warn("error creating new request")
-				return
+			// A method for sending the request with a specific accept header value
+			doRequest := func(accept string) (*http.Response, error) {
+				req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
+				if err != nil {
+					log.WithError(err).Warn("error creating new request")
+					return nil, err
+				}
+				for key, values := range header {
+					req.Header[key] = values
+				}
+				req.Header.Set("Accept", accept)
+				return m.httpClientGetHeader.Do(req)
 			}
-
-			// Extend the request header with our values
-			for key, values := range header {
-				req.Header[key] = values
-			}
-
-			// Get the optional version, used with SSZ decoding
-			ethConsensusVersion := header.Get("Eth-Consensus-Version")
-			log = log.WithField("ethConsensusVersion", ethConsensusVersion)
 
 			// Send the get bid request to the relay. Try what the client
 			// accepts (either SSZ or JSON) first. If no accept type is specified,
 			// request JSON. We cannot request SSZ if the client does not, because
 			// the appropriate Eth-Consensus-Version header value is not known.
+			var err error
 			var resp *http.Response
-			acceptFromClient := req.Header.Get("Accept")
-			switch acceptFromClient {
+			switch header.Get("Accept") {
 			case "application/octet-stream":
 				log.Debug("requesting header in SSZ")
-				req.Header.Set("Accept", "application/octet-stream")
-				resp, err = m.httpClientGetHeader.Do(req)
+				resp, err = doRequest("application/octet-stream")
+				if err != nil {
+					log.WithError(err).Warn("error sending request")
+					return
+				}
+				defer resp.Body.Close()
+
+				// Check if the relay supports SSZ requests
 				if resp.StatusCode != http.StatusNotAcceptable {
 					// The relay didn't complain about the accept value.
 					// This means we should try processing the response.
 					log.Debug("response indicated SSZ is accepted")
 					break
 				}
+
 				// The response status was NotAcceptable.
 				// This means we should try again with JSON.
 				log.Debug("response indicated SSZ is not accepted")
 				fallthrough
 			default:
 				log.Debug("requesting header in JSON")
-				req.Header.Set("Accept", "application/json")
-				resp, err = m.httpClientGetHeader.Do(req)
+				resp, err = doRequest("application/json")
+				defer resp.Body.Close()
 			}
 			if err != nil {
 				log.WithError(err).Warn("error calling getHeader on relay")
 				return
 			}
-			defer resp.Body.Close()
 
 			// Check if no header is available
 			if resp.StatusCode == http.StatusNoContent {
