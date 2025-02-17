14442d14a33fccda42c37c78b7e6b20cd3f84be4
Justin Traglia <jtraglia@pm.me>
2025-02-17 08:55:20 -0600

    Fix linter

--- server/service_test.go
+++ server/service_test.go
@@ -370,7 +370,7 @@ func TestGetHeader(t *testing.T) {
 		rr := backend.request(t, http.MethodGet, path, header, nil)
 		require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
 		require.Equal(t, 1, backend.relays[0].GetRequestCount(path))
-		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType))
+		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType)) //nolint:testifylint
 	})
 
 	t.Run("Two relays return JSON, mev-boost returns JSON", func(t *testing.T) {
@@ -385,7 +385,7 @@ func TestGetHeader(t *testing.T) {
 		require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
 		require.Equal(t, 1, backend.relays[0].GetRequestCount(path))
 		require.Equal(t, 1, backend.relays[1].GetRequestCount(path))
-		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType))
+		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType)) //nolint:testifylint
 	})
 
 	t.Run("Accepts both with Q values", func(t *testing.T) {
@@ -410,7 +410,7 @@ func TestGetHeader(t *testing.T) {
 		rr := backend.request(t, http.MethodGet, path, header, nil)
 		require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
 		require.Equal(t, 1, backend.relays[0].GetRequestCount(path))
-		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType))
+		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType)) //nolint:testifylint
 	})
 
 	t.Run("Accepts both but prefers JSON", func(t *testing.T) {
@@ -422,7 +422,7 @@ func TestGetHeader(t *testing.T) {
 		rr := backend.request(t, http.MethodGet, path, header, nil)
 		require.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
 		require.Equal(t, 1, backend.relays[0].GetRequestCount(path))
-		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType))
+		require.Equal(t, MediaTypeJSON, rr.Header().Get(HeaderContentType)) //nolint:testifylint
 	})
 
 	t.Run("Only accepts unsupported media type", func(t *testing.T) {
