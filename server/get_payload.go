4f2575bef7c74b662278711fa27dc76ea5e2a751
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2025-04-18 17:31:39 -0600

    refactor: rename 'contentType' to 'requestContentType' in getPayload relay req

--- server/get_payload.go
+++ server/get_payload.go
@@ -158,11 +158,11 @@ func (m *BoostService) getPayload(log *logrus.Entry, signedBlindedBeaconBlockByt
 			// If the request fails, try again a few times with 100ms between tries
 			resp, err := retry(requestCtx, m.requestMaxRetries, 100*time.Millisecond, func() (*http.Response, error) {
 				// If necessary, use the JSON encoded version and the JSON Content-Type header
-				contentType := parsedProposerContentType
+				requestContentType := parsedProposerContentType
 				requestBytes := signedBlindedBeaconBlockBytes
 				if parsedProposerContentType == MediaTypeOctetStream && !relay.SupportsSSZ {
 					requestBytes = signedBlindedBeaconBlockBytesJSON
-					contentType = MediaTypeJSON
+					requestContentType = MediaTypeJSON
 				}
 
 				// Make a new request
@@ -174,7 +174,7 @@ func (m *BoostService) getPayload(log *logrus.Entry, signedBlindedBeaconBlockByt
 
 				// Add header fields to this request
 				req.Header.Set(HeaderAccept, proposerAcceptContentTypes)
-				req.Header.Set(HeaderContentType, contentType)
+				req.Header.Set(HeaderContentType, requestContentType)
 				req.Header.Set(HeaderEthConsensusVersion, proposerEthConsensusVersion)
 				req.Header.Set(HeaderKeySlotUID, currentSlotUID)
 				req.Header.Set(HeaderDateMilliseconds, fmt.Sprintf("%d", time.Now().UTC().UnixMilli()))
