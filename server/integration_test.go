8562163a786e68fcb7d0c8c5cdfaef971d7a93a4
faheelsattar <faheelsattar@yahoo.com>
2025-08-26 00:17:34 +0200

    upd

--- server/integration_test.go
+++ server/integration_test.go
@@ -388,6 +388,7 @@ func TestMEVBoostIntegration(t *testing.T) {
 		var payloads []ProposerPayloadDelivered
 		err = json.Unmarshal(body, &payloads)
 		require.NoError(t, err)
+		require.NotEmpty(t, payloads)
 
 		currentSlot, _ := beaconClient.GetCurrentSlot()
 		nextSlot := currentSlot + 1
